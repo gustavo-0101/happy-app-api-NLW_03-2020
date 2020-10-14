76d7ce92b3856fb3c3281395414dd618680c4583
Gustavo Dias <guga-rd@hotmail.com>
2020-10-14 18:06:22 -0300

    commit da tarde

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,40 @@
 import express from "express";
+import { getRepository } from "typeorm";
+
+import Orphanage from "./models/Orphanage";
+
+import "./database/conection";
 
 const app = express();
 
-app.get("/users", (req, res) => {
-  return res.json({ message: "Hello World" });
+app.use(express.json());
+
+app.post("/orphanages", async (req, res) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    open_on_weekends,
+    opening_hours,
+  } = req.body;
+
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    open_on_weekends,
+    opening_hours,
+  });
+
+  await orphanagesRepository.save(orphanage);
+
+  return res.json(orphanage);
 });
 
 app.listen(3333);
