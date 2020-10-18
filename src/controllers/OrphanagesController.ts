b60b5864fc2e6a34eb5e9227eff8a59ae52d2712
gustavo-0101 <guga-rd@hotmail.com>
2020-10-18 02:43:01 -0300

    backend concluded

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -55,7 +55,7 @@ export default {
       longitude,
       about,
       instructions,
-      open_on_weekends,
+      open_on_weekends: open_on_weekends === 'true',
       opening_hours,
       images,
     };
